08c3b67e404f7090f9b59abc8269c03e99889640
Matheus Trindade <trindadematheus27@gmail.com>
2022-06-18 14:25:43 -0300

    first

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
