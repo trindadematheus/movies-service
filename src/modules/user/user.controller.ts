8264f3a2f37264f8afe83bc1c83552aff2ed0139
Matheus Trindade <trindadematheus27@gmail.com>
2022-06-18 16:37:51 -0300

    status code

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
@@ -26,16 +27,19 @@ export class UserController {
   }
 
   @Post()
+  @HttpCode(201)
   create(@Body() createUserDTO: CreateUserDTO) {
     return this.userService.create(createUserDTO);
   }
 
   @Put(':id')
+  @HttpCode(204)
   update(@Param('id') id: string, @Body() updateUserDTO: UpdateUserDTO) {
     return this.userService.update(id, updateUserDTO);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   destroy(@Param('id') id: string) {
     return this.userService.destroy(id);
   }
