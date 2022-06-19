ab4954fa8780022bac3f723f0f6d29d5d9c9f214
Matheus Trindade <trindadematheus27@gmail.com>
2022-06-18 22:28:17 -0300

    validate fields

--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,19 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
 
 export class QueryDTO {
-  page?: string;
-  take?: string;
-  order?: 'asc' | 'desc';
-  admin?: 'all' | 'true' | 'false';
-  active?: 'all' | 'true' | 'false';
+  readonly page?: string;
+  readonly take?: string;
+  readonly order?: 'asc' | 'desc';
+  readonly admin?: 'all' | 'true' | 'false';
+  readonly active?: 'all' | 'true' | 'false';
 }
 
 export class CreateUserDTO {
+  @IsString()
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
   readonly password: string;
+
+  @IsBoolean()
   readonly is_admin: boolean;
-  readonly is_active: boolean;
 }
 
-export class UpdateUserDTO extends PartialType(CreateUserDTO) {}
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {
+  @IsBoolean()
+  readonly is_active?: boolean;
+}
--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -11,7 +11,7 @@ export class User {
   @Column({ unique: true })
   email: string;
 
-  @Column()
+  @Column({ select: false })
   password: string;
 
   @Column()
